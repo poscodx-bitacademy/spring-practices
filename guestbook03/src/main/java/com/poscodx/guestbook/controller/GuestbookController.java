66033e4d70b2685942d82f163fa18f5c36e099e7
포스코DX-비트아카데미 <141541801+poscodx-bitacademy@users.noreply.github.com>
2023-09-15 14:44:47 +0900

    Update GuestbookController.java

--- guestbook03/src/main/java/com/poscodx/guestbook/controller/GuestbookController.java
+++ guestbook03/src/main/java/com/poscodx/guestbook/controller/GuestbookController.java
@@ -4,5 +4,31 @@ import org.springframework.stereotype.Controller;
 
 @Controller
 public class GuestbookController {
+	@Autowired
+	private GuestbookRepository guestbookRepository;
+	
+	@RequestMapping(value="/", method=RequestMethod.GET)
+	public String index(Model model) {
+		List<GuestbookVo> list = guestbookRepository.findAll();
+		model.addAttribute("list", list);
+		return "/WEB-INF/views/index.jsp";
+	}
 
+	@RequestMapping("/add")
+	public String add(GuestbookVo vo) {
+		guestbookRepository.insert(vo);
+		return "redirect:/";
+	}
+	
+	@RequestMapping(value="/delete/{no}", method=RequestMethod.GET)
+	public String delete(@PathVariable("no") Long no, Model model) {
+		model.addAttribute("no", no);
+		return "/WEB-INF/views/delete.jsp";
+	}
+
+	@RequestMapping(value="/delete/{no}", method=RequestMethod.POST)
+	public String delete(@PathVariable("no") Long no, @RequestParam(value="password", required=true, defaultValue="") String password) {
+		guestbookRepository.deleteByNoAndPassword(no, password);
+		return "redirect:/";
+	}
 }
