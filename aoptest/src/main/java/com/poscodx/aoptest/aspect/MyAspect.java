d75e7d664e397dd41ff8109463dab21bb664605f
poscodx-bitacademy <poscodx.bitacademy@gmail.com>
2023-09-19 10:56:06 +0900

    aoptest #2

---
+++ aoptest/src/main/java/com/poscodx/aoptest/aspect/MyAspect.java
@@ -0,0 +1,35 @@
+package com.poscodx.aoptest.aspect;
+
+import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.AfterThrowing;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.springframework.stereotype.Component;
+
+
+@Component
+@Aspect
+public class MyAspect {
+	
+	@Before("execution(public com.poscodx.aoptest.vo.ProductVo com.poscodx.aoptest.service.ProductService.find(String))")
+	public void adviceBefore() {
+		System.out.println("---- Before Advice ----");
+	}
+	
+	@After("execution(com.poscodx.aoptest.vo.ProductVo com.poscodx.aoptest.service.ProductService.find(String))")
+	public void adviceAfter() {
+		System.out.println("---- After Advice ----");
+	}
+
+	@AfterReturning("execution(* com.poscodx.aoptest.service.ProductService.find(String))")
+	public void adviceAfterReturning() {
+		System.out.println("---- AfterReturning Advice ----");
+	}
+
+	@AfterThrowing(value="execution(* *..*.ProductService.find(..))", throwing="ex")
+	public void adviceAfterThrowing(Throwable ex) {
+		System.out.println("---- AfterThrowing Advice:" + ex + " ----");
+	}
+	
+}
