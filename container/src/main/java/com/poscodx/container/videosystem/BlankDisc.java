73216a6ace95309fb17cd42103101d107f868b40
poscodx-bitacademy <poscodx.bitacademy@gmail.com>
2023-10-05 11:53:07 +0900

    Explicit - (Xml config) #2

--- container/src/main/java/com/poscodx/container/videosystem/BlankDisc.java
+++ container/src/main/java/com/poscodx/container/videosystem/BlankDisc.java
@@ -7,6 +7,9 @@ public class BlankDisc implements DigitalVideoDisc {
 	private String studio;
 	private List<String> actors;
 	
+	public BlankDisc() {
+	}
+	
 	public BlankDisc(String title, String studio) {
 		this.title = title;
 		this.studio = studio;
@@ -15,6 +18,18 @@ public class BlankDisc implements DigitalVideoDisc {
 	public BlankDisc(String title, String studio, List<String> actors) {
 		this.title = title;
 		this.studio = studio;
+		this.actors = actors;
+	}
+	
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public void setStudio(String studio) {
+		this.studio = studio;
+	}
+
+	public void setActors(List<String> actors) {
 		this.actors = actors;
 	}
 
